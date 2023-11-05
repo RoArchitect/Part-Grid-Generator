5db961352d01da9e11a27aa5d47fc2851b1e0d0b
RoArchitect <145255543+RoArchitect@users.noreply.github.com>
2023-11-05 04:47:37 -0600

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,15 @@
+-- << Creating your first grid >> --
+local NewGrid : Table = GridModule.new(Vector3.new(0.5,0.5,0.5) , 0.2 , 32)
+--[[
+	[1] = BLOCK_SIZE; : Vector3
+	[2] = BLOCK_SPACING; : Number
+	[3] = BLOCK_AMOUNT; : Number
+]]
+
+local GeneratedGrid : Table = NewGrid:Generate()
+local GridBlock : Part = GeneratedGrid.GetGridBlock(1023)
+--[[
+To get the total amount of grid blocks, just do your BLOCK_AMOUNT multiplied by BLOCK_AMOUN
+T
+GenerateGrid.GetGridBlock(1023) -- 1023 being the grid block number (1-1024 if it's 32x32 )
+]]
